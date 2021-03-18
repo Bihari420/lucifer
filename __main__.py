8573fe175f76abf28b90c98b42172bc48efd9604
Adarsh Kumar <64361893+Bihari420@users.noreply.github.com>
2021-03-18 23:14:02 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -133,13 +133,13 @@ def send_start(bot, update):
         pass
 
     #chat = update.effective_chat  # type: Optional[Chat] and unused variable
-    text = "Hey there! My name is Punisher - I'm here to help you manage your groups!\n\
+    text = "Welcome to Wasseypur, Myself Sardar Khan - I'm here to help you manage your groups!\n\
 Click Help button to find out more about how to use me to my full potential.\n\n"
 
-    text += "Follow ( @Punishersupport ) if you want to keep up with the news, updates and bot downtime!\n\n\
-Mainatined by @Nitin181\n\nWant to add me to your group? [Click here!](t.me/MrPunisher_bot?startgroup=true)"
+    text += "Follow ( @sardarkhaaan ) if you want to keep up with the news, updates and bot downtime!\n\n\
+Mainatined by @ChevroletImpala67\n\nWant to add me to your group? [Click here!](t.me/Sardarkhaaan_bot?startgroup=true)"
 
-    keyboard = [[InlineKeyboardButton(text="Updates", url="https://t.me/punishersupport")]]
+    keyboard = [[InlineKeyboardButton(text="Updates", url="https://t.me/sardarkhaaan")]]
     keyboard += [[InlineKeyboardButton(text="Language", callback_data="set_lang_"), 
         InlineKeyboardButton(text="Help", callback_data="help_back")]]
 
